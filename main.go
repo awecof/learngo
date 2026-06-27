96d662221ecbe853c81bcc69f58e11579408f49f
awecof <yujihun20060217@gmail.com>
2026-06-27 11:44:24 +0900

    Finishing Up

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"fmt"
 	"learngo/accounts"
-	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("jihun")
 	account.Deposit(10)
-	err := account.Withdraw(20)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
