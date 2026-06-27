db40be4652a669d1360bc4fd43a3e106aaaff21c
awecof <yujihun20060217@gmail.com>
2026-06-27 11:36:28 +0900

    Methods part Two

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 type Account struct {
 	owner   string
 	balance int
@@ -10,10 +12,20 @@ func NewAccount(owner string) *Account {
 	return &account
 }
 
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 func (a Account) Balance() int {
 	return a.balance
+}
+
+var NoMoney = errors.New("can't withdraw you are poor")
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return NoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"learngo/accounts"
+	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("jihun")
 	account.Deposit(10)
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }
