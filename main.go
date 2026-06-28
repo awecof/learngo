970b10896fd8675adbcb928cfc75569aa92fc0d7
awecof <yujihun20060217@gmail.com>
2026-06-28 20:39:34 +0900

    URLChecker + Go Routines

--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type result struct {
+	url    string
+	status string
+}
+
 func main() {
-	c := make(chan string)
-	people := [2]string{"jihun", "nico"}
-	for _, person := range people {
-		go isSexy(person, c)
+	results := map[string]string{}
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println("Waiting for", i)
-		fmt.Println(<-c)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for _, result := range results {
+		fmt.Println(result)
 	}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
+func hitURL(url string, c chan<- result) error {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	fmt.Println("Succeeded")
+	c <- result{url: url, status: status}
 }
