c1e4d2adef1f459107f50b4025341b6ebec8501e
awecof <yujihun20060217@gmail.com>
2026-06-28 20:05:46 +0900

    Channels

--- main.go
+++ main.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	c := make(chan bool)
+	people := [2]string{"jihun", "nico"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-			fmt.Println("error")
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
