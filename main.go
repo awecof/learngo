2f2d07ed397d7454b017a91b3aa4a9a45e8c9580
awecof <yujihun20060217@gmail.com>
2026-06-29 21:42:49 +0900

    extractJob part One

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type extractedJob struct {
+	value    string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
 
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
 	totalPages := getPages()
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		getPage(i + 1)
 	}
 }
 
 func getPage(page int) {
-
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	searchCards := doc.Find(".item_recruit")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		value, _ := card.Attr("value")
+		title := card.Find(".area_job>.job_tit>a").Text()
+		location := card.Find(".area_job>.job_condition>span>a").Text()
+		fmt.Println("https://www.saramin.co.kr/zf_user/jobs/relay/view?rec_idx=" + value)
+		fmt.Println("title:", title, "location:", location)
+	})
 }
 
 func getPages() int {
